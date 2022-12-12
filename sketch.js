2451ddd7222aabea993ffae83bbf4f238413e2e8
Karola0525 <114681978+Karola0525@users.noreply.github.com>
2022-12-12 17:20:23 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+
+
+
+function preload() {
+
+}
+function setup() {
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
+
+ 
+}
+
+function draw() {
+  background(189);
+  Engine.update(engine);
+
+  
+   
+}
